c1a1e792aa116d5cee2479d11d22808d09404f0d
Lgkvlv <lgkvlv.01@gmail.com>
2025-03-31 16:41:36 +0300

    Update views.py

--- tgminiapp/App/views.py
+++ tgminiapp/App/views.py
@@ -1,13 +1,26 @@
 from rest_framework import viewsets, permissions
+from rest_framework.authentication import TokenAuthentication  # Добавляем импорт
 from .models import Category, Transaction
 from .serializers import CategorySerializer, TransactionSerializer
 
 class CategoryViewSet(viewsets.ModelViewSet):
+    authentication_classes = [TokenAuthentication]  # Добавляем аутентификацию
     permission_classes = [permissions.IsAuthenticated]
     serializer_class = CategorySerializer
-    queryset = Category.objects.all()
+    
+    def get_queryset(self):
+        """Возвращаем только категории текущего пользователя"""
+        return Category.objects.filter(user=self.request.user)
 
 class TransactionViewSet(viewsets.ModelViewSet):
+    authentication_classes = [TokenAuthentication]  # Добавляем аутентификацию
     permission_classes = [permissions.IsAuthenticated]
     serializer_class = TransactionSerializer
-    queryset = Transaction.objects.all()
+    
+    def get_queryset(self):
+        """Возвращаем только транзакции текущего пользователя"""
+        return Transaction.objects.filter(user=self.request.user)
+    
+    def perform_create(self, serializer):
+        """Автоматически привязываем пользователя при создании"""
+        serializer.save(user=self.request.user)
