a59011167b6b5933fc0e6a517cc3718ea09f613e
Lgkvlv <lgkvlv.01@gmail.com>
2025-03-31 18:29:22 +0300

    Update models.py

--- tgminiapp/App/models.py
+++ tgminiapp/App/models.py
@@ -1,28 +1,17 @@
 from django.db import models
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
 
 class Category(models.Model):
+    user_id = models.BigIntegerField()
     name = models.CharField(max_length=100)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    is_income = models.BooleanField(default=False)
-
-    class Meta:
-        verbose_name_plural = "Categories"
-        unique_together = ('name', 'user')
 
     def __str__(self):
         return self.name
 
-
 class Transaction(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user_id = models.BigIntegerField()
     amount = models.DecimalField(max_digits=10, decimal_places=2)
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
-    description = models.TextField(blank=True)
-    date = models.DateField()
-    created_at = models.DateTimeField(auto_now_add=True)
+    category = models.CharField(max_length=100)
+    date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.amount} - {self.category}"
+        return f"{self.category}: {self.amount} ₽"
